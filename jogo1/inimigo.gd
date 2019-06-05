34c83afc0a3fc8258b687f09a2be9380b56f718a
clbk <apenasjogos@yahoo.com.br>
2019-06-05 10:00:55 -0300

    criado 'inimigo'

---
+++ jogo1/inimigo.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+export var velocidade_min =150
+export var velocidade_max = 250
+
+var tipos_inimigos = ["carro", "carroLouco", "coelho"]
+
+func _ready():
+	#escolher aleatoriamento o inimigo
+	$AnimatedSprite.animation = tipos_inimigos[randi() % tipos_inimigos.size()]
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free() #quando o sinal eh emitido ("saiu da tela") o inimigo é excluido da tela
